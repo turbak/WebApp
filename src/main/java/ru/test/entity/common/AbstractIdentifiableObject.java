9538a2f15989724b52ef39749be7ceef1673e182
ishcherbina <ishcherbina@phoenixit.ru>
2020-02-27 13:27:27 +0300

    Added advanced architecture that doesn't work yet

---
+++ src/main/java/ru/test/entity/common/AbstractIdentifiableObject.java
@@ -0,0 +1,43 @@
+package ru.test.entity.common;
+
+import javax.persistence.Transient;
+import java.io.Serializable;
+
+public abstract class AbstractIdentifiableObject<Id extends Serializable> implements Identifiable<Id> {
+	protected Id id;
+	@Transient
+	private Integer hashcode;
+
+	@Override
+	public Id getId() {
+		return id;
+	}
+
+	@Override
+	public void setId(Id id) {
+		this.id = id;
+	}
+
+	@Override
+	public synchronized int hashCode() {
+		if (hashcode == null) {
+			hashcode = id.hashCode() + 31;
+		}
+		return hashcode;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null)
+			return false;
+		if (o.getClass() != this.getClass())
+			return false;
+		AbstractIdentifiableObject<?> obj = (AbstractIdentifiableObject<?>)o;
+		if (id == null) {
+			return obj.id == null;
+		}
+		else return id.equals(obj.id);
+	}
+}
